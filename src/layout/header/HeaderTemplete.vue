0e423dfd88e594beca74aab8800f6a563714acf9
JC-GIRALDO-GIRALDO <kmilom19@gmail.com>
2023-10-20 12:02:30 -0500

    agregue logo en el header

---
+++ src/layout/header/HeaderTemplete.vue
@@ -0,0 +1,21 @@
+<script setup>
+</script>
+
+<template>
+
+  <img src="../../../public//tvm-logo-black.png" alt="logo-tvm">
+  <ul>
+    <li>textto-1</li>
+    <li>textto-2</li>
+    <li>textto-3</li>
+    <li>textto-4</li>
+  </ul>
+  <h2>login</h2>
+  <h2>contacto</h2>
+  <h2>idioma</h2>
+
+</template>
+
+<style scoped>
+
+</style>
