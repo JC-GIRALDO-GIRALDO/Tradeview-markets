358b64c73a792638da48bf67b836afbf28259705
JC-GIRALDO-GIRALDO <kmilom19@gmail.com>
2023-10-21 21:59:36 -0500

    cree el componente de container-community, agregue texto, agregue botones, aplique colores

--- src/layout/footer/CommunityTraders.vue
+++ src/layout/footer/CommunityTraders.vue
@@ -1,9 +1,50 @@
-<script setup></script>
-
 <template>
-  <h2>hola</h2>
+  <div class="container-community">
+    <div class="container-section-community">
+      <section class="container text-center py-3 center-title">
+        <h3>Try CommuniTraders Now!</h3>
+      </section>
+      <section class="container">
+        <div class="row">
+          <div class="col-md-6 d-flex justify-content-center">
+            <div class="text-center mx-auto">
+              <!-- Agregamos mx-auto para centrar horizontalmente -->
+              <p>Free Download for Tradeview Users</p>
+              <button class="btn btn-danger">Contact your Broker</button>
+            </div>
+          </div>
+          <div class="col-md-6 d-flex justify-content-center border-start">
+            <div class="text-center mx-auto">
+              <!-- Agregamos mx-auto para centrar horizontalmente -->
+              <p>Don't have a Tradeview Account yet?</p>
+              <button class="btn btn-danger">Get a Free Demo</button>
+            </div>
+          </div>
+        </div>
+      </section>
+    </div>
+  </div>
 </template>
 
 <style scoped>
-
+.container-community {
+  background-color: #181f29;
+}
+.container-section-community {
+  padding: 2rem;
+}
+.border-start {
+  border-left: 1px solid #ffffff; /* Añade una línea vertical en el medio */
+}
+.center-title h3 {
+  margin-top: 3rem;
+  margin-bottom: 1.5rem;
+  color: #ffffff;
+}
+.text-center p {
+  color: #ffffff;
+}
+.text-center button {
+  margin-bottom: 3rem;
+}
 </style>
